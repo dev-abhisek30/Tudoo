be47772a89d8dad856eaa85e25dfb06b36eb4532
abhisek30 <abhisekcsce@gmail.com>
2022-03-05 18:36:44 +0530

    Base Project

---
+++ app/src/main/java/com/dev/simplified/tudoo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dev.simplified.tudoo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
