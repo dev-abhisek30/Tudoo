4f395a33bece292c7f9307443c62fdbba3eafb92
dev-abhisek30 <dev.abhisek30@gmail.com>
2022-03-06 00:12:51 +0530

    Integrated Bottom Navigation Bar and Calendar View

--- app/src/main/java/com/dev/simplified/tudoo/HomeFragment.kt
+++ app/src/main/java/com/dev/simplified/tudoo/HomeFragment.kt
@@ -1,20 +1,238 @@
 package com.dev.simplified.tudoo
 
+import android.animation.ValueAnimator
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.ColorRes
+import androidx.annotation.RequiresApi
+import androidx.core.animation.doOnEnd
+import androidx.core.animation.doOnStart
+import androidx.core.content.ContextCompat
+import androidx.core.view.children
+import androidx.core.view.updateLayoutParams
+import com.dev.simplified.tudoo.databinding.CalendarDayLayoutBinding
+import com.dev.simplified.tudoo.databinding.FragmentHomeBinding
+import com.kizitonwose.calendarview.model.CalendarDay
+import com.kizitonwose.calendarview.model.DayOwner
+import com.kizitonwose.calendarview.model.InDateStyle
+import com.kizitonwose.calendarview.ui.DayBinder
+import com.kizitonwose.calendarview.ui.ViewContainer
+import com.kizitonwose.calendarview.utils.next
+import com.kizitonwose.calendarview.utils.yearMonth
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.time.temporal.WeekFields
+import java.util.*
 
-
+@RequiresApi(Build.VERSION_CODES.O)
 class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
+    private var selectedDates: LocalDate? = null
+    private val today = LocalDate.now()
+    private val monthTitleFormatter = DateTimeFormatter.ofPattern("MMMM")
+    private val selectionFormatter = DateTimeFormatter.ofPattern("d MMM yyyy")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        val daysOfWeek = daysOfWeekFromLocale()
+        binding.legendLayout.root.children.forEachIndexed { index, view ->
+            (view as TextView).apply {
+                text = daysOfWeek[index].getDisplayName(TextStyle.SHORT, Locale.ENGLISH).toUpperCase(Locale.ENGLISH)
+                setTextColorRes(R.color.calender_blue)
+            }
+        }
+
+        class DayViewContainer(view: View) : ViewContainer(view) {
+            //val textView = view.findViewById<TextView>(R.id.calendarDayText)
+
+            // With ViewBinding
+            lateinit var day: CalendarDay
+            val textView = CalendarDayLayoutBinding.bind(view).calendarDayText
+
+            init {
+                view.setOnClickListener {
+                    /*if (day.owner == DayOwner.THIS_MONTH) {
+                        if (selectedDates.contains(day.date)) {
+                            selectedDates.remove(day.date)
+                        } else {
+                            selectedDates.add(day.date)
+                        }
+                        binding.exOneCalendar.notifyDayChanged(day)
+                    }*/
+                    if (day.owner == DayOwner.THIS_MONTH) {
+                        selectDate(day.date)
+                    }
+                    binding.exOneCalendar.notifyDayChanged(day)
+                }
+            }
+        }
+
+        val currentMonth = YearMonth.now()
+        val startMonth = currentMonth.minusMonths(10)
+        val endMonth = currentMonth.plusMonths(10)
+        val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
+        binding.exOneCalendar.setup(startMonth, endMonth, daysOfWeek.first())
+        binding.exOneCalendar.scrollToMonth(currentMonth)
+
+        binding.exOneCalendar.dayBinder = object : DayBinder<DayViewContainer> {
+            // Called only when a new container is needed.
+            override fun create(view: View) = DayViewContainer(view)
+
+            // Called every time we need to reuse a container.
+
+            override fun bind(container: DayViewContainer, day: CalendarDay) {
+                container.day = day
+                val textView = container.textView
+                textView.text = day.date.dayOfMonth.toString()
+
+                if (day.owner == DayOwner.THIS_MONTH) {
+                    when {
+                        selectedDates == day.date -> {
+                            textView.setTextColorRes(R.color.example_3_blue)
+                            textView.setBackgroundResource(R.drawable.calendar_selected_bg)
+                        }
+                        today == day.date -> {
+                            textView.setTextColorRes(R.color.example_3_white)
+                            textView.setBackgroundResource(R.drawable.calendar_today_bg)
+                        }
+                        else -> {
+                            textView.setTextColorRes(R.color.example_3_black)
+                            textView.background = null
+                        }
+                    }
+                } else {
+                    textView.setTextColorRes(R.color.calender_blue)
+                    textView.background = null
+                }
+            }
+        }
+
+        binding.exOneCalendar.monthScrollListener = {
+            if (binding.exOneCalendar.maxRowCount == 6) {
+                binding.exOneYearText.text = it.yearMonth.year.toString()
+                binding.exOneMonthText.text = monthTitleFormatter.format(it.yearMonth)
+            } else {
+                val firstDate = it.weekDays.first().first().date
+                val lastDate = it.weekDays.last().last().date
+                if (firstDate.yearMonth == lastDate.yearMonth) {
+                    binding.exOneYearText.text = firstDate.yearMonth.year.toString()
+                    binding.exOneMonthText.text = monthTitleFormatter.format(firstDate)
+                } else {
+                    binding.exOneMonthText.text =
+                        "${monthTitleFormatter.format(firstDate)} - ${monthTitleFormatter.format(lastDate)}"
+                    if (firstDate.year == lastDate.year) {
+                        binding.exOneYearText.text = firstDate.yearMonth.year.toString()
+                    } else {
+                        binding.exOneYearText.text = "${firstDate.yearMonth.year} - ${lastDate.yearMonth.year}"
+                    }
+                }
+            }
+        }
+        binding.weekModeCheckBox.setOnCheckedChangeListener { _, monthToWeek ->
+            val firstDate = binding.exOneCalendar.findFirstVisibleDay()?.date ?: return@setOnCheckedChangeListener
+            val lastDate = binding.exOneCalendar.findLastVisibleDay()?.date ?: return@setOnCheckedChangeListener
+
+            val oneWeekHeight = binding.exOneCalendar.daySize.height
+            val oneMonthHeight = oneWeekHeight * 6
+
+            val oldHeight = if (monthToWeek) oneMonthHeight else oneWeekHeight
+            val newHeight = if (monthToWeek) oneWeekHeight else oneMonthHeight
+
+            // Animate calendar height changes.
+            val animator = ValueAnimator.ofInt(oldHeight, newHeight)
+            animator.addUpdateListener { animator ->
+                binding.exOneCalendar.updateLayoutParams {
+                    height = animator.animatedValue as Int
+                }
+            }
+            animator.doOnStart {
+                if (!monthToWeek) {
+                    binding.exOneCalendar.updateMonthConfiguration(
+                        inDateStyle = InDateStyle.ALL_MONTHS,
+                        maxRowCount = 6,
+                        hasBoundaries = true
+                    )
+                }
+            }
+            animator.doOnEnd {
+                if (monthToWeek) {
+                    binding.exOneCalendar.updateMonthConfiguration(
+                        inDateStyle = InDateStyle.FIRST_MONTH,
+                        maxRowCount = 1,
+                        hasBoundaries = false
+                    )
+                }
+
+                if (monthToWeek) {
+                    binding.exOneCalendar.scrollToDate(firstDate)
+                } else {
+                    if (firstDate.yearMonth == lastDate.yearMonth) {
+                        binding.exOneCalendar.scrollToMonth(firstDate.yearMonth)
+                    } else {
+                        binding.exOneCalendar.scrollToMonth(minOf(firstDate.yearMonth.next, endMonth))
+                    }
+                }
+            }
+            animator.duration = 250
+            animator.start()
+        }
+
+        return view
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    fun daysOfWeekFromLocale(): Array<DayOfWeek> {
+        val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
+        var daysOfWeek = DayOfWeek.values()
+        if (firstDayOfWeek != DayOfWeek.MONDAY) {
+            val rhs = daysOfWeek.sliceArray(firstDayOfWeek.ordinal..daysOfWeek.indices.last)
+            val lhs = daysOfWeek.sliceArray(0 until firstDayOfWeek.ordinal)
+            daysOfWeek = rhs + lhs
+        }
+        return daysOfWeek
+    }
+
+    private fun selectDate(date: LocalDate) {
+        if (selectedDates != date) {
+            val oldDate = selectedDates
+            selectedDates = date
+            oldDate?.let { binding.exOneCalendar.notifyDateChanged(it) }
+            binding.exOneCalendar.notifyDateChanged(date)
+            updateAdapterForDate(date)
+        }
+    }
+
+    private fun updateAdapterForDate(date: LocalDate) {
+        /*eventsAdapter.apply {
+            events.clear()
+            events.addAll(this@Example3Fragment.events[date].orEmpty())
+            notifyDataSetChanged()
+        }*/
+        //binding.exThreeSelectedDateText.text = selectionFormatter.format(date)
+        Log.d("-Here--",selectionFormatter.format(date).toString())
+    }
+
+
+    private fun TextView.setTextColorRes(@ColorRes color: Int) = setTextColor(context.getColorCompat(color))
+    private fun Context.getColorCompat(@ColorRes color: Int) = ContextCompat.getColor(this, color)
 }
--- app/src/main/java/com/dev/simplified/tudoo/MainActivity.kt
+++ app/src/main/java/com/dev/simplified/tudoo/MainActivity.kt
@@ -2,10 +2,36 @@ package com.dev.simplified.tudoo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.dev.simplified.tudoo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var navHostFragment: NavHostFragment
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.bottomNavigationView.itemIconTintList = null
+        binding.bottomNavigationView.clearAnimation()
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.addFragment || destination.id == R.id.updateFragment) {
+                binding.bottomNavigationView.visibility = View.GONE
+            } else {
+                binding.bottomNavigationView.visibility = View.VISIBLE
+            }
+        }
+
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
